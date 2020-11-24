cee6c8373972d39950864e8397143ee02990308d
Lars-Erik Aabech <lea@markedspartner.no>
2020-11-24 22:07:21 +0100

    Test actual output. 🤦‍♂️

--- src/core/test/logic-apps-to-plantuml.spec.js
+++ src/core/test/logic-apps-to-plantuml.spec.js
@@ -1,18 +1,15 @@
-const { verify } = require('crypto');
-
 require('approvals').mocha();
 const fs = require('fs');
+const logic2puml = require('../logicapp-to-plantuml');
 
 describe("converting logic apps to plantuml", function() {
 
     it("generates full activity diagram", function() {
 
         const input = fs.readFileSync('./test/logicapp.json');
-        const obj = JSON.parse(input);
-        console.log(obj);
+        const puml = logic2puml.convert(input);
 
-        this.verify(obj);
+        this.verify(puml);
 
     });
-
 });
